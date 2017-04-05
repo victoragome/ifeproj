1fd662e212fd8e2ef630c4bf0383174c5829256b
victoragome <victoragome@gmail.com>
2017-04-05 12:42:25 +0100

    first commit

---
+++ src/main/java/com/mycompany/ifeproj/NewServlet.java
@@ -0,0 +1,137 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ifeproj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author victor
+ */
+public class NewServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<h1>Servlet NewServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        } finally {            
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+            StringBuilder strBuf = new StringBuilder();  
+        
+        HttpURLConnection conn=null;
+        BufferedReader reader=null;
+        System.out.println("inside post");
+        try{  
+            String urlString = "https://api.yelp.com/v3/autocomplete?text=rel";
+            //Declare the connection to weather api url
+            URL url = new URL(urlString);  
+            conn = (HttpURLConnection)url.openConnection();  
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+            //conn.setRequestProperty("apikey",apiKey);
+            System.out.println("inside post");
+              
+           // if (conn.getResponseCode() != 200) {
+           //     throw new RuntimeException("HTTP GET Request Failed with Error code : "
+           //                   + conn.getResponseCode());
+          //  }
+            System.out.println("inside post");
+            //Read the content from the defined connection
+            //Using IO Stream with Buffer raise highly the efficiency of IO
+	        reader = new BufferedReader(new InputStreamReader(conn.getInputStream(),"utf-8"));
+            String output = null;  
+            while ((output = reader.readLine()) != null)  
+                strBuf.append(output);  
+        }catch(MalformedURLException e) {  
+            e.printStackTrace();   
+        }catch(IOException e){  
+            e.printStackTrace();   
+        }
+        finally
+        {
+            if(reader!=null)
+            {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(conn!=null)
+            {
+                conn.disconnect();
+            }
+        }
+
+        System.out.println(strBuf.toString());  
+        //processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
